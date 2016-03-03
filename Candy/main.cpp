4b441dac371b2efc3b8564c310cc2a9d6e798153
mangonob <534393806@qq.com>
2016-03-03 22:11:07 +0800

    Initial Commit

---
+++ Candy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Candy
+//
+//  Created by Trinity on 16/3/3.
+//  Copyright © 2016年 Trinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
